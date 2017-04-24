667ceef2e633519dd161ad5ae062bfcfd197c910
Administrator <Administrator@it-pc>
2017-04-24 14:34:09 +0800

    2017-4-24 14:34

--- amazing/src/amazing/core/Env.java
+++ amazing/src/amazing/core/Env.java
@@ -3,7 +3,9 @@ package amazing.core;
 public class Env {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Env!!");
+		while(true){
+			System.out.println("1");
+		}
 	}
 
 }
